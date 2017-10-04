2eec58b6ff9cc608ac19cbc33bdf9353f7932a1d
mattbellis <matthew.bellis@gmail.com>
2017-10-04 01:55:06 -0400

    Added new data and some new files for testing.

--- pyncorr/general.py
+++ pyncorr/general.py
@@ -1,1 +1,44 @@
 import numpy as np
+
+################################################################################
+def write_out_paircounts(counts, bin_edges, n0, n1, filename="output.dat", norm=None):
+
+    if norm==None:
+        norm = n0*n1
+
+    bin_lo = bin_edges[0:-1]
+    bin_hi = bin_edges[1:]
+
+    bin_centers = bin_lo + (bin_hi - bin_lo)
+
+    # We add some zeros for padding, just to make reading in the files
+    # a bit easier later on. 
+    output = "%d 0 0 0\n" % (n0)
+    output += "%d 0 0 0\n" % (n1)
+    output += "%d 0 0 0\n" % (norm)
+
+    for a,b,c,d in zip(bin_lo, bin_centers, bin_hi, counts):
+
+        output += "%f %f %f %d\n" % (a,b,c,d)
+
+    outfile = open(filename,'w+')
+
+    outfile.write(output)
+
+    outfile.close()
+
+
+################################################################################
+def radeccmd2xyz(ra, dec, cmd):
+
+    # com is comoving distance
+    rarad = np.deg2rad(ra)
+    decrad = np.deg2rad(dec)
+
+    x=cmd*np.cos(ra)*np.cos(dec)
+    y=cmd*np.sin(ra)*np.cos(dec)
+    z=cmd*np.sin(dec)
+
+    return x,y,z
+
+
--- scripts/testpyncorr.py
+++ scripts/testpyncorr.py
@@ -2,53 +2,75 @@ import numpy as np
 import pyncorr
 import matplotlib.pylab as plt
 
-nd = 1000
-nr = 1000
+nbins = 200
 
-x = np.random.random((nd,3))
-y = np.random.random((nr,3))
+#nd = 1000
+#nr = 1000
+#x = np.random.random((nd,3))
+#y = np.random.random((nr,3))
 
-d,edges = pyncorr.cartesian_distance(x,y,histrange=(0,1.5))
-dr = d
-print(d)
-print(sum(d))
-print()
-print(edges)
+# Read in some data
+data = np.loadtxt('../test_data/GRID_model_data.dat',unpack=True)
+random = np.loadtxt('../test_data/GRID_model_random.dat',unpack=True)
+#data = np.loadtxt('../test_data/GRID_model_data_LARGE_SAMPLE.dat',unpack=True)
+#random = np.loadtxt('../test_data/GRID_model_random_LARGE_SAMPLE.dat',unpack=True)
 
-print()
+nd = len(data[0])
+nr = len(random[0])
 
-d,edges = pyncorr.cartesian_distance(x,x,histrange=(0,1.5),same_coords=True)
-dd = d
-print(d)
-print(sum(d))
-print()
-print(edges)
+#data = pyncorr.radeccmd2xyz(data[0],data[1],data[2])
+#data = np.array(data)
+data = data.transpose()
 
-d,edges = pyncorr.cartesian_distance(y,y,histrange=(0,1.5),same_coords=True)
-rr = d
-print(d)
-print(sum(d))
-print()
-print(edges)
+#random = pyncorr.radeccmd2xyz(random[0],random[1],random[2])
+#random = np.array(random)
+random = random.transpose()
 
-bin_centers = edges[0:-1] + (edges[1:] - edges[0:-1])
+x = data
+y = random
+hrange = 450
 
 plt.figure()
-plt.plot(bin_centers,dr,label='dr')
-plt.plot(bin_centers,dd,label='dd')
-plt.plot(bin_centers,rr,label='rr')
-plt.legend()
+plt.plot(x.transpose()[0],x.transpose()[1],',')
 
-ddnorm = (nd*nd - nd)/2.0
-rrnorm = (nr*nr - nr)/2.0
-drnorm = nr*nd
+
+d,edges = pyncorr.cartesian_distance(x,y,histrange=(0,hrange),nbins=nbins)
+dr = d
+pyncorr.write_out_paircounts(d,edges,nd,nr,filename='dr.dat',norm=nd*nr)
+
+print()
+
+d,edges = pyncorr.cartesian_distance(x,x,histrange=(0,hrange),same_coords=True,nbins=nbins)
+dd = d
+pyncorr.write_out_paircounts(d,edges,nd,nd,filename='dd.dat',norm=(nd*nd-nd)/2.0)
+
+d,edges = pyncorr.cartesian_distance(y,y,histrange=(0,hrange),same_coords=True,nbins=nbins)
+rr = d
+pyncorr.write_out_paircounts(d,edges,nr,nr,filename='rr.dat',norm=(nr*nr-nr)/2.0)
+
+
+# Read in the data
+dd_data = np.loadtxt('dd.dat',unpack=True)
+dr_data = np.loadtxt('dr.dat',unpack=True)
+rr_data = np.loadtxt('rr.dat',unpack=True)
+
+# Pull out the bin centers from the first one.
+x = dd_data[1][3:]
+# Get the pair counts and normalizations
+dd,ddnorm = dd_data[3][3:],dd_data[0][2]
+dr,drnorm = dr_data[3][3:],dr_data[0][2]
+rr,rrnorm = rr_data[3][3:],rr_data[0][2]
+
+plt.figure()
+plt.plot(x,dr,'o',label='dr')
+plt.plot(x,dd,'o',label='dd')
+plt.plot(x,rr,'o',label='rr')
+plt.legend()
 
 w = ((dd/ddnorm) - (2*dr/drnorm) + (rr/rrnorm))/(rr/rrnorm)
 
 plt.figure()
-plt.plot(bin_centers,w,label='w')
+plt.plot(x,w,'o',label='w')
 plt.legend()
-
-
 
 plt.show()
