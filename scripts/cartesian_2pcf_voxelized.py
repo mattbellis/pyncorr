c1ca3848e3c7aa4c80949030222d36ecee178098
mattbellis <matthew.bellis@gmail.com>
2018-06-05 02:43:58 -0400

    Distance on MPI is working

--- scripts/cartesian_2pcf_voxelized.py
+++ scripts/cartesian_2pcf_voxelized.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pyncorr
 import matplotlib.pylab as plt
 
 import time 
--- scripts/test_sandbox/mpi_distance_test.py
+++ scripts/test_sandbox/mpi_distance_test.py
@@ -3,6 +3,10 @@ from scipy import spatial
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 
+import sys
+sys.path.append('../../pyncorr')
+from pyncorr import cartesian_distance
+
 comm = MPI.COMM_WORLD
 rank = comm.rank
 size = comm.size
@@ -12,8 +16,11 @@ size = comm.size
 #seed = np.random.seed(4)
 #rand = np.random.random((1000,3))
 
-data = np.loadtxt("../../test_data/GRID_model_data.dat")
-rand = np.loadtxt("../../test_data/GRID_model_random.dat")
+#data = np.loadtxt("../../test_data/GRID_model_data.dat")
+#rand = np.loadtxt("../../test_data/GRID_model_random.dat")
+
+data = np.loadtxt("../../test_data/GRID_model_data_LARGE_SAMPLE.dat")
+rand = np.loadtxt("../../test_data/GRID_model_random_LARGE_SAMPLE.dat")
 
 #print(data[0],rand[0])
 
@@ -30,19 +37,22 @@ for i in range(rank,4,size):
     #DD
     if rank is 0:
         t_dd_i=MPI.Wtime()
-        dd = spatial.distance.pdist(data)
-        h_dd = np.histogram(dd,bins=100)
-        hist_dd += h_dd[0]
+        #dd = spatial.distance.pdist(data)
+        #h_dd = np.histogram(dd,bins=100)
+        #hist_dd += h_dd[0]
+        hist_dd,bin_edges = cartesian_distance(data,data,nbins=100,histrange=(0,10),same_coords=True)
         t_dd_f=MPI.Wtime()-t_dd_i
     #RR
     if rank is 1:
-        rr = spatial.distance.pdist(rand)
-        h_rr = np.histogram(rr,bins=100)
-        hist_rr += h_rr[0]
+        #rr = spatial.distance.pdist(rand)
+        #h_rr = np.histogram(rr,bins=100)
+        #hist_rr += h_rr[0]
+        hist_rr,bin_edges = cartesian_distance(rand,rand,nbins=100,histrange=(0,10),same_coords=True)
     #DR
-    dr = spatial.distance.cdist(ddiv[i],rand)
-    h_dr = np.histogram(dr,bins=100)
-    hist_dr += h_dr[0]
+    #dr = spatial.distance.cdist(ddiv[i],rand)
+    #h_dr = np.histogram(dr,bins=100)
+    #hist_dr += h_dr[0]
+    hist_dr,bin_edges = cartesian_distance(ddiv[i],rand,nbins=100,histrange=(0,10),same_coords=True)
     comm.Barrier()
 
 hist_dr_f = np.zeros_like(hist_dr)
